ea9f327c23f02d63a7dbe5cd8572a0ad58e802f6
parkerkary <parker.kary@fiixsoftware.com>
2018-11-06 14:25:51 -0500

    updated restcontroller

---
+++ python/Controller/RestController.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/call-tensor-flow',methods=["POST"])
+def getConfidence():
+    content = request.json
+    tfData = callTensorFlow(content)
+    return 1
+
+def callTensorFlow(content):
+    print content
+    return content == "hello_world"
