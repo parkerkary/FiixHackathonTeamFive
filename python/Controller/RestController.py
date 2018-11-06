cfeb05a5d67421018f92f0b23d7c4fadf4c0aa9c
parkerkary <parker.kary@fiixsoftware.com>
2018-11-06 14:27:46 -0500

    fixed bug in restcontroller

--- python/Controller/RestController.py
+++ python/Controller/RestController.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/call-tensor-flow',methods=["POST"])
+
+@app.route('/call-tensor-flow', methods=["POST"])
 def getConfidence():
     content = request.json
     tfData = callTensorFlow(content)
     return 1
 
+
 def callTensorFlow(content):
-    print content
+    print(content)
     return content == "hello_world"
