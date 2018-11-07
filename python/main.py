f140567e25d74478acf6fb4ab0eea9a702763b8c
parkerkary <parker.kary@fiixsoftware.com>
2018-11-07 12:23:45 -0500

    updated ip and port

--- python/main.py
+++ python/main.py
@@ -2,4 +2,4 @@ from Scraper.WebScraper import WebScraper
 from Controller import RestController
 
 if __name__ == "__main__":
-    RestController.app.run()
+    RestController.app.run(host="18.188.113.98",port="8080")
